9ef30822e441edd065e29949a5c52bbd2ea3ab69
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-03-18 22:47:49 +0100

    Fix email expiration task

--- lib/tasks/check_emails_for_expire.rb
+++ lib/tasks/check_emails_for_expire.rb
@@ -11,14 +11,14 @@ class CheckEmailsForExpire
         email.lock_access!
       end
       # save email send in emails if successfull
-      email.set_reminder_sent(true)
+      email.reminder_sent = true
       puts "#{Time.now}: reminder email was send for #{email.email}"
     end
 
     # deactivate emails that did not response to the remainder
-    @emails_to_deactivate = Email.get_emails_expired
+    @emails_to_deactivate = Email.to_be_expired
     @emails_to_deactivate.each do |email|
-      email.deactivate
+      email.active = false
       puts "#{Time.now}: email: #{email.email} was deactivated"
     end
   end
