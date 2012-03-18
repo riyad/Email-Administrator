385f332d2e6e15acb1f3c5722df01a9e79b19393
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-03-18 21:07:37 +0100

    Rename conf option to background_task_interval

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -6,7 +6,7 @@ require('./lib/tasks/check_emails_for_expire.rb')
  
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every(Rails.env == "test" ? "23h" : APP_CONFIG["email_background_task"]) do
+scheduler.every(Rails.env == "test" ? "23h" : APP_CONFIG["background_task_interval"]) do
  puts "#{Time.now}: Parse emails logs"
  LogParser.new 
  puts "#{Time.now}: Check activity of emails"
