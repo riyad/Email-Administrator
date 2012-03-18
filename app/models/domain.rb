5fe71e3cf96a68ab432d11c617529ca6f54d49b6
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-03-18 21:34:43 +0100

    Add default scope to domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,8 @@
 class Domain < ActiveRecord::Base
   attr_accessible :name
   has_many :emails
+
+  default_scope order("name")
   
   validates :name, :presence => true, :format => { :with => /^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/i,
     :message => "%{value} has invalid format" }
