0cffd22e316493342389eb145d7cbe9bb5e59e82
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-03-18 21:34:30 +0100

    Fix has_many relation in domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,6 +1,6 @@
 class Domain < ActiveRecord::Base
   attr_accessible :name
-  has_many :domains
+  has_many :emails
   
   validates :name, :presence => true, :format => { :with => /^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$/i,
     :message => "%{value} has invalid format" }
