baa9c80121f2f6cc82ad6043b9117dfe9711250d
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2012-03-19 14:55:39 +0100

    Reordered methods in email

--- app/models/email.rb
+++ app/models/email.rb
@@ -27,36 +27,18 @@ class Email < ActiveRecord::Base
   before_validation :convert_email
 
   #static methods
+  def forwards
+    forward_email.try(:split, " ") || []
+  end
+
+  def forwards=(array_or_string)
+    array_or_string = array_or_string.split(/\s+/) if array_or_string.is_a? String
+
+    self.forward_email = array_or_string.map(&:downcase).uniq.join(" ")
+  end
+
   def self.get_emails_expires_soon
     Email.where("expires_on <= ? and reminder_sent = ? and active = ? and admin = ?",(Date.today + 14.days),false,true,false)
-  end
-  
-  def prefix
-    email.split("@").first if email
-  end
-
-  def self.search(search)
-    if search
-      where('email LIKE ?', "%#{search}%")
-    else
-      scoped
-    end
-  end
-  
-  public
-  
-  def domain=(value)
-    update_attribute(:domain_id , value.id)
-    update_attribute(:email ,get_email_prefix(self[:email]) + "@" + value.name)
-  end
-
-  def send_unlock_instructions
-    EmailMailer.expires_email(self).deliver
-  end
-  
-  def send_reset_password_instructions
-    generate_reset_password_token! if should_generate_reset_token?
-    EmailMailer.reset_password_instructions(self).deliver
   end
   
   def password_salt=(password_salt)
@@ -68,7 +50,30 @@ class Email < ActiveRecord::Base
   def password_digest(password)
     password.crypt("2a")
   end
-  
+
+  def prefix
+    email.split("@").first if email
+  end
+
+  def self.search(search)
+    if search
+      where('email LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
+
+  def send_reset_password_instructions
+    generate_reset_password_token! if should_generate_reset_token?
+    EmailMailer.reset_password_instructions(self).deliver
+  end
+
+  def send_unlock_instructions
+    EmailMailer.expires_email(self).deliver
+  end
+
+  end
+
   alias :devise_valid_password? :valid_password?
   def valid_password?(password)
     if self.admin
@@ -78,16 +83,6 @@ class Email < ActiveRecord::Base
         password.crypt(encrypted_password[0..2]) == encrypted_password
       end
     end
-  end
-
-  def forwards
-    forward_email.try(:split, " ") || []
-  end
-
-  def forwards=(array_or_string)
-    array_or_string = array_or_string.split(/\s+/) if array_or_string.is_a? String
-
-    self.forward_email = array_or_string.map(&:downcase).uniq.join(" ")
   end
   
   private
